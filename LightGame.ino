866b2d5720f961290669a6ab8f721b6a7c8b4c12
Lee Ming Rui <mingz@Lees-MacBook-Pro-2.local>
2023-10-03 01:44:17 -0400

    Defining conditions for win, lose and comparison

--- LightGame.ino
+++ LightGame.ino
@@ -1,7 +1,11 @@
 int targetIntensity;
 int buzzer = 16;
 int sensor = 36;
-int currentIntensity = 0;
+int myTime = 0;
+int currentIntensity = 0; // because i need to collect the data
+int diffIntensity = 0; // because i have no idea what the value is yet
+int pitch;
+bool gameEnded = false;
 
 void setup() {
   Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
@@ -19,28 +23,39 @@ void setup() {
 
 void loop() {
 
-  // check if 10s has passed
-  Serial.print("Time: ");
-  myTime = millis();
-  Serial.println(myTime); // prints time since program started
+  if (!gameEnded){
+    // check if 10s has passed
+    Serial.print("Stopwatch: ");
+    myTime = millis();
+    Serial.println(myTime); // prints time since program started
 
-  // read light intensity at sensor
-  currentIntensity = analogRead(sensor);
-  Serial.printIn(currentIntensity);
+    // read light intensity at sensor
+    currentIntensity = analogRead(sensor);
+    Serial.print("Current Intensity: ");
+    Serial.println(currentIntensity);
 
-  // compare light intensity read with targeted intensity
-  
 
-  // if number gets closer, buzzer plays higher pitch
+    // if number gets closer, buzzer plays higher pitch
+    // if number gets further, buzzer plays lower pitch
+    diffIntensity = abs(targetIntensity - currentIntensity);
+    pitch = map(diffIntensity,0,4095,2000,31);
+    tone(buzzer, pitch);
 
-  // if number gets further, buzzer plays lower pitch
+    // if win, play win melody
+    if (myTime <= 10000 && currentIntensity == targetIntensity){
+      playWinMelody();
+      gameEnded = true;
+      Serial.println("You win!");
+      return;
+    }
 
-  // if win, play win melody
-  
-
-  // if lose, play loser melody
-  if (myTime > 10000) {
-    playLoserMelody();
+    // if lose, play loser melody
+    if (myTime > 10000) {
+      playLoserMelody();
+      gameEnded = true;
+      Serial.println("You lose!");
+      return;
+    }
   }
 
   // press key to restart
@@ -60,3 +75,4 @@ void playWinMelody(){
 void playLoserMelody(){
 
 }
+
