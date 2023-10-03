99faad59d80c337b8dd5bad5540e1f1767df6d7f
Lee Ming Rui <mingz@Lees-MacBook-Pro-2.local>
2023-10-02 23:56:29 -0400

    Implement set up and analogRead

--- LightGame.ino
+++ LightGame.ino
@@ -1,41 +1,62 @@
-int randNumber;
+int targetIntensity;
 int buzzer = 16;
 int sensor = 36;
+int currentIntensity = 0;
 
 void setup() {
   Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
   randomSeed(analogRead(0));
+
+  // generate random number
+  targetIntensity = random(4096); // print a random number from 0 to 4095
+  Serial.println("The target intensity is..."); 
+  Serial.println(targetIntensity); 
+
+  // play simple melody
+  playStartMelody();
+
 }
 
 void loop() {
 
-
-
-  // generate random number
-  randNumber = random(4096); // print a random number from 0 to 4095
-  Serial.println(randNumber);
-
-  // play simple melody
-
-  // start 10s countdown
+  // check if 10s has passed
+  Serial.print("Time: ");
+  myTime = millis();
+  Serial.println(myTime); // prints time since program started
 
   // read light intensity at sensor
+  currentIntensity = analogRead(sensor);
+  Serial.printIn(currentIntensity);
 
   // compare light intensity read with targeted intensity
+  
 
   // if number gets closer, buzzer plays higher pitch
 
   // if number gets further, buzzer plays lower pitch
 
   // if win, play win melody
+  
 
   // if lose, play loser melody
+  if (myTime > 10000) {
+    playLoserMelody();
+  }
 
   // press key to restart
 
 
 
+}
 
+void playStartMelody(){
+  // TODO
+}
 
+void playWinMelody(){
+
+}
+
+void playLoserMelody(){
 
 }
